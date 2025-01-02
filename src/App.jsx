455a899b6823153829b81e377c8d190d934ed6cf
Andrey Cherny-Tkach <achernytkach@gmail.com>
2025-01-02 15:24:52 +0400

    step_3.4.1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from "./components/Greeting";
+
+function App() {
+  return (
+    <div>
+      <Greeting name="Alice" />
+      <Greeting name="Bob" />
+      <Greeting name="Rob" />
+    </div>
+  );
+}
+
+export default App;
